db1ba3ab5725f8d09c317a2aa2a01a5e45021100
eb8686 <ericbertrand7575@gmail.com>
2023-05-31 15:17:43 +0200

    feat : urls dbug + migrate

--- AppCantine/AppCantine/urls.py
+++ AppCantine/AppCantine/urls.py
@@ -25,17 +25,17 @@ urlpatterns = [
 # Use include() to add paths from the catalog application
 from django.urls import include
 urlpatterns += [
-    path('catalog/', include('catalog.urls')),
+    path('cantinedata/', include('cantinedata.urls')),
 ]
 
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='cantinedata/', permanent=True)),
 ]
 
 # Use static() to add URL mapping to serve static files during development (only)
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
